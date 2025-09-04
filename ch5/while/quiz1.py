95854478acde3dbf58e7b0486da8922d0f18ba08
klop57 <klop575257@gmail.com>
2025-09-04 17:07:24 +0900

    first commit

---
+++ ch5/while/quiz1.py
@@ -0,0 +1,32 @@
+# 반복문을 사용해서 "하이" 3번 출력하세요
+
+i = 0
+while i < 3:
+    print("하이")
+    i = i + 1
+# 반복문을 사용해서 0~4까지 출력하세요
+
+i = 0
+while i < 4:
+    print("하이")
+    i = i + 1
+
+print(0)
+print(1)
+print(2)
+print(3)
+print(4)
+
+# 반복문을 사용해서 1~5까지 출력하세요
+
+i = 0
+while i < 5:
+    print(i)
+    i=i+1
+# 반복문을 사용해서 11부터 20까지 출력하세요
+# 
+i = 11
+while i <= 20:
+    print(i)
+    i=i+1
+      
---
+++ ch5/while/quiz2.py
@@ -0,0 +1,33 @@
+# 다음과 같이 리스트를 만들고
+# 반복문으로 요소를 하나씩 출력하세요
+lis = ['a','b','c','d','e']
+
+print(lis[0])
+print(lis[1])
+print(lis[2])
+print(lis[3])
+print(lis[4])
+# 반복횟수 : 5
+# 반복코드 : print(lis[?])
+
+i = 0
+while i < 5:
+    print(lis[i])
+    i+=1
+
+# 다음과 같이 리스트를 만들고
+# 반복문으로 요소를 하나씩 출력하세요
+nums = [10,20,30]
+
+print(nums[0])
+print(nums[1])
+print(nums[2])
+
+# i가 2부터 4이 될ㄸ까지
+# 총 3번 수행
+i = 2
+while i <= 4:
+    print(nums[i-2])
+    i+=1
+
+
